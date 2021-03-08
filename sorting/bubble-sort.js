8578c5b16d05a5f149d99ed86799597096d16e73
zhengminhui <zhmh1025@hotmail.com>
2021-03-08 11:41:10 +0800

    add merge sort

--- sorting/bubble-sort.js
+++ sorting/bubble-sort.js
@@ -1,46 +1,25 @@
-function swap(items, i, j) {
-  const temp = items[i];
-  items[i] = items[j];
-  items[j] = temp;
+function swap(arr, i, j) {
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
 }
 
 /**
  * In-place sort array, index count up from head to tail.
  * @param {*} arr An array of items needs to be sorted.
+ * O(n*n)
  */
 function bubbleSort(arr) {
-  const len = arr.length;
-  const stop = len - 1;
-
-  for (let i = 0; i < len; i++) {
-    for (let j = 0; j < stop; j++) {
+  // 外层i遍历全部，每一轮会把最大的数字移到队尾
+  for (let i = 0; i < arr.length - 1; i++) {
+    // 内层 j 不用到最后，last i elements are already in place
+    for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1);
       }
     }
   }
-
   return arr;
 }
 
-/**
- * In-place sort array. Use two reversed loops count down from tail to head.
- * @param {*} arr An array of items needs to be sorted.
- */
-function bubbleSort2(arr) {
-  const len = arr.length;
-
-  for (let i = len - 1; i > 0; i--) {
-    for (let j = len - 1; j > 0; j--) {
-      if (arr[j] < arr[j - 1]) {
-        console.log(arr[j], arr[j - 1]);
-        swap(arr, j, j - 1);
-      }
-    }
-  }
-
-  return arr;
-}
-
-console.log(bubbleSort([1, 3, 2, 4, 9, 8, 2, 6, 7])); // [ 1, 2, 2, 3, 4, 6, 7, 8, 9 ]
-console.log(bubbleSort2([1, 3, 2, 4, 9, 8, 2, 6, 7]));
+console.log(bubbleSort([1, 3, 2, 4, 9, 8, 2, 6, 7, 1]));
